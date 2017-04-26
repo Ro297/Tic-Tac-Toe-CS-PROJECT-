94aaa8bc628bf73020c89152bb95d59273845c63
Ro297 <rohan.poddar_ug19@ashoka.edu.in>
2017-04-26 22:47:15 +0530

    Update TIC TAC TOE BOARD.py

--- TIC TAC TOE BOARD.py
+++ TIC TAC TOE BOARD.py
@@ -1,18 +1,16 @@
-import random
-
 #Drawing the board for the tic tac toe game.
-def drawBoard(board):
+def drawTTTBoard(pos):
     print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print(' ' + pos[1] + ' | ' + pos[2] + ' | ' + pos[3])
     print('   |   |')
     print('-----------')
     print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(' ' + pos[4] + ' | ' + pos[5] + ' | ' + pos[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + pos[7] + ' | ' + pos[8] + ' | ' + pos[9])
     print('   |   |')
 #We are ignoring the 0th index in order to avoid confusion. So code shouldn't fill anything in the 0th place.
 
-drawBoard([' ','0','X','X','0',' ',' ',' ',' ',' ']) 
+drawTTTBoard([' ','0','X','X','0',' ',' ',' ',' ',' ']) 
